e3ade3dc9844534c5e6f1559c4141be6c7cb39ae
habib2013 <nairabet25@gmail.com>
2021-05-06 13:09:21 +0100

    --- Auto-Git Commit ---

--- resources/views/emails/mails/welcome.blade.php
+++ resources/views/emails/mails/welcome.blade.php
@@ -3,7 +3,8 @@
 
 Hi {{ $userwallet->first_name }} , 
 Welcome to a world of more convenience.
-With the Capital X wallet, you can enjoy unlimited free bank transfers, high interest on savings and hassle free bills payments.
+With the Capital X wallet, you can enjoy unlimited free bank transfers,
+ high interest on savings and hassle free bills payments.
 You can upgrade your account by ordering for your card or opting for a Premium plan
 Need help? Kindly reach our support team via support@capitalx.cards or 0043482725.
 Once again, welcome onboard!
