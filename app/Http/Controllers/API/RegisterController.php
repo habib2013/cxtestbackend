25ca05b30efced4d92a414af94b1fccea91b59ab
habib2013 <nairabet25@gmail.com>
2021-05-14 11:41:31 +0100

    --- Auto-Git Commit ---

--- app/Http/Controllers/API/RegisterController.php
+++ app/Http/Controllers/API/RegisterController.php
@@ -99,6 +99,7 @@ public function verifyReference($reference){
     CURLOPT_URL => "https://api.paystack.co/transaction/verify/".$reference,
     CURLOPT_RETURNTRANSFER => true,
     CURLOPT_ENCODING => "",
+    
     CURLOPT_MAXREDIRS => 10,
     CURLOPT_TIMEOUT => 60,
     CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
